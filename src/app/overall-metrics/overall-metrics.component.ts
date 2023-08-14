0a42e917ab49c2de5e9263efca19f8dd86e9c770
mealthebear <mealthebear@gmail.com>
2023-08-14 04:34:33 -0700

    Initialized overall-metrics component

---
+++ src/app/overall-metrics/overall-metrics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-metrics',
+  templateUrl: './overall-metrics.component.html',
+  styleUrls: ['./overall-metrics.component.css']
+})
+export class OverallMetricsComponent {
+
+}
