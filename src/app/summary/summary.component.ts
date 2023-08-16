392016ca4b7982eec5946cbef1e36b788e3547dc
mealthebear <mealthebear@gmail.com>
2023-08-16 04:42:52 -0700

    Added totaled values to bottom of the grid

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -21,6 +21,20 @@ export class SummaryComponent {
 
   summaryDataGrid: RowData[] = [];
   layout: string[] = [];
+  totaledDataRow: string[] = [
+    'Total',
+    '',
+    '',
+    '0',
+    '0',
+    '',
+    '0',
+    '0',
+    '',
+    '0',
+    '0',
+    '',
+  ];
 
   getSummaryData(): void {
     this.summaryService.getSummaryData().subscribe((summaryData) => {
@@ -47,36 +61,88 @@ export class SummaryComponent {
         newRowOfData.push(dataset[row]['channel']);
         newRowOfData.push(dataset[row]['segment']);
         newRowOfData.push(dataset[row]['tactic']);
+
+        this.totaledDataRow[3] = `${
+          Number(Number(dataset[row]['totalSpend']).toFixed(2)) +
+          Number(this.totaledDataRow[3])
+        }`;
         newRowOfData.push(
           this.numberFormatter(dataset[row]['totalSpend'], 'currency')
         );
+
+        this.totaledDataRow[4] = `${
+          Number(Number(dataset[row]['mediaSpend']).toFixed(2)) +
+          Number(this.totaledDataRow[4])
+        }`;
         newRowOfData.push(
           this.numberFormatter(dataset[row]['mediaSpend'], 'currency')
         );
+
         newRowOfData.push(
           this.numberFormatter(dataset[row]['percInc'], 'percent')
         );
+
+        this.totaledDataRow[6] = `${
+          Number(Number(dataset[row]['salesLT']).toFixed(2)) +
+          Number(this.totaledDataRow[6])
+        }`;
         newRowOfData.push(
           this.numberFormatter(dataset[row]['salesLT'], 'currency')
         );
+
+        this.totaledDataRow[7] = `${
+          Number(Number(dataset[row]['salesI']).toFixed(2)) +
+          Number(this.totaledDataRow[7])
+        }`;
         newRowOfData.push(
           this.numberFormatter(dataset[row]['salesI'], 'currency')
         );
+
         newRowOfData.push(
           this.numberFormatter(dataset[row]['percSalesI'], 'percent')
         );
+
+        this.totaledDataRow[9] = `${
+          Number(Number(dataset[row]['ordersLT']).toFixed(2)) +
+          Number(this.totaledDataRow[9])
+        }`;
         newRowOfData.push(
           this.numberFormatter(dataset[row]['ordersLT'], 'number')
         );
+
+        this.totaledDataRow[10] = `${
+          Number(Number(dataset[row]['ordersI']).toFixed(2)) +
+          Number(this.totaledDataRow[10])
+        }`;
         newRowOfData.push(
           this.numberFormatter(dataset[row]['ordersI'], 'number')
         );
+
         newRowOfData.push(
           this.numberFormatter(dataset[row]['percOrdersI'], 'percent')
         );
         dataGrid.push(newRowOfData);
       }
     }
+    this.totaledDataRow[3] = `$${Number(
+      Number(this.totaledDataRow[3]).toFixed(2)
+    )}`;
+    this.totaledDataRow[4] = `$${Number(
+      Number(this.totaledDataRow[4]).toFixed(2)
+    )}`;
+    this.totaledDataRow[6] = `$${Number(
+      Number(this.totaledDataRow[6]).toFixed(2)
+    )}`;
+    this.totaledDataRow[7] = `$${Number(
+      Number(this.totaledDataRow[7]).toFixed(2)
+    )}`;
+    this.totaledDataRow[9] = `${Number(
+      Number(this.totaledDataRow[9]).toFixed(2)
+    )}`;
+    this.totaledDataRow[10] = `${Number(
+      Number(this.totaledDataRow[10]).toFixed(2)
+    )}`;
+    console.log(this.totaledDataRow);
     return dataGrid;
   }
 
